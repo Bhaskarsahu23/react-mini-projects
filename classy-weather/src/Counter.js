765369d044b56204ff5eeead49780020b024228d
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-07-14 22:02:28 +0530

    add project classy weather

---
+++ classy-weather/src/Counter.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-useless-constructor */
+import React from 'react';
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 5,
+    };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return {
+        count: curState.count + 1,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <button>{this.state.count}</button>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
