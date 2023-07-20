bae697bca9f74a335ab255c6d11bfbd6692c8c7b
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-07-20 22:18:20 +0530

    add more functionalities

--- worldwise/src/components/AppNav.jsx
+++ worldwise/src/components/AppNav.jsx
@@ -1,9 +1,18 @@
+import { NavLink } from 'react-router-dom';
 import styles from './AppNav.module.css';
 
 function AppNav() {
   return (
-    <div className={styles.appnav}>
-      <h1>App Nav</h1>
+    <div className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from '../components/AppNav';
+import SideBar from '../components/SideBar';
+import Map from '../components/Map';
 import styles from './AppLayout.module.css';
+
 function AppLayout() {
   return (
     <div className={styles.app}>
-      <AppNav />
-      <h1>Hello</h1>
+      <SideBar />
+      <Map />
     </div>
   );
 }
