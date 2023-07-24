0550266a43a5256a8034f56fb64ee5fa12992779
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-07-24 21:36:31 +0530

    fix some codes

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import Product from './pages/Product';
@@ -10,6 +10,7 @@ import PageNotFound from './pages/PageNotFound';
 import CityList from './components/CityList';
 import City from './components/City';
 import CountryList from './components/CountryList';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:9000/cities';
 
@@ -40,10 +41,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -53,7 +51,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
