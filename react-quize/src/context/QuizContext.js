39dd97f053d7af919d2adb24deecf9c9aa42b0e2
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-08-04 21:16:07 +0530

    add new context QuizeContext

---
+++ react-quize/src/context/QuizContext.js
@@ -0,0 +1,104 @@
+import { useContext, createContext, useReducer } from 'react';
+
+const quizContext = createContext();
+
+const SECS_PER_Questions = 30;
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
+        secondsRemaining: state.questions.length * SECS_PER_Questions,
+      };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
+    case 'finish':
+      return {
+        ...state,
+        status: 'finished',
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+    case 'restart':
+      return {
+        ...initialState,
+        status: 'ready',
+        questions: state.questions,
+      };
+    case 'tick':
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? 'finished' : state.status,
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
+function QuizContextProvider({ children }) {
+  const [
+    { questions, status, index, answer, points, highScore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  return (
+    <quizContext.Provider
+      value={{
+        questions,
+        status,
+        index,
+        answer,
+        points,
+        highScore,
+        secondsRemaining,
+        dispatch,
+      }}
+    >
+      {children}
+    </quizContext.Provider>
+  );
+}
+
+function useQuize() {
+  const context = useContext(quizContext);
+
+  if (context === undefined) throw new Error('using context outside provider');
+
+  return context;
+}
+
+export { QuizContextProvider, useQuize };
