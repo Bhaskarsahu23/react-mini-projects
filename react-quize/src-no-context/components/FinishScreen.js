8ae2f57b8a2339c5d3e4be298650b2d2f56fe9af
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-08-04 21:15:08 +0530

    old file with no context

---
+++ react-quize/src-no-context/components/FinishScreen.js
@@ -0,0 +1,29 @@
+const scored = ({ points, maxPossiblePoints, highScore, dispatch }) => {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+
+  if (percentage === 100) emoji = '🎖';
+  if (percentage >= 80 && percentage < 100) emoji = '🎉';
+  if (percentage >= 50 && percentage < 80) emoji = '🎆';
+  if (percentage >= 0 && percentage < 50) emoji = '🎇';
+  if (percentage === 0) emoji = '😢';
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>
+        YOU scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)})
+      </p>
+      <p className="highscore">(HighScore: {highScore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'restart' })}
+      >
+        restart
+      </button>
+    </>
+  );
+};
+export default scored;
