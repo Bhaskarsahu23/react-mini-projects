df5bd020b9a85b4b5703655f73f9ac0e320bd755
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-12-20 13:23:22 +0530

    complete contact us page

---
+++ 3d-portfolio-website/src/components/Alert.jsx
@@ -0,0 +1,22 @@
+function Alert({ type, text }) {
+  return (
+    <div className="absolute top-10 left-0 right-0 flex justify-center items-center">
+      <div
+        className={`${
+          type === 'danger' ? 'bg-red-800' : 'bg-green-800'
+        } p-2 text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex`}
+        role="alert"
+      >
+        <p
+          className={`${
+            type === 'danger' ? 'bg-red-500' : 'bg-green-500'
+          } flex rounded-full uppercase font-semibold mr-3 px-2 py-1 text-xs`}
+        >
+          {type === 'danger' ? 'Failed' : 'Success'}
+        </p>
+        <p className="mr-2 text-left">{text}</p>
+      </div>
+    </div>
+  );
+}
+export default Alert;
--- 3d-portfolio-website/src/models/IsLand.jsx
+++ 3d-portfolio-website/src/models/IsLand.jsx
@@ -107,16 +107,16 @@ export function Island({
         ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
 
       switch (true) {
-        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+        case normalizedRotation >= 5.12 && normalizedRotation <= 5.85:
           setCurrentStage(4);
           break;
         case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
           setCurrentStage(3);
           break;
-        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+        case normalizedRotation >= 0.05 && normalizedRotation <= 2.6:
           setCurrentStage(2);
           break;
-        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+        case normalizedRotation >= 2.8 && normalizedRotation <= 4.85:
           setCurrentStage(1);
           break;
         default:
